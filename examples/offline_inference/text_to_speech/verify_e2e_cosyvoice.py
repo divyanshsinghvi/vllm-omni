b655d395f61d81d97028d9372d710f8ad7dc269b
Divyansh Singhvi <divyanshsinghvi@gmail.com>
2026-01-01 12:13:40 +0530

    Fix precommit
    Signed-off-by: Divyansh Singhvi <divyanshsinghvi@gmail.com>

--- examples/offline_inference/text_to_speech/verify_e2e_cosyvoice.py
+++ examples/offline_inference/text_to_speech/verify_e2e_cosyvoice.py
@@ -4,11 +4,11 @@ import os
 import librosa
 import numpy as np
 import soundfile as sf
+from vllm import SamplingParams
 
 # Use Omni entrypoint directly
 from vllm.assets.audio import AudioAsset
 
-from vllm import SamplingParams
 from vllm_omni.entrypoints.omni import Omni
 from vllm_omni.model_executor.models.cosyvoice3.config import CosyVoice3Config
 from vllm_omni.model_executor.models.cosyvoice3.tokenizer import get_qwen_tokenizer
